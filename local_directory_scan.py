79b3ee26a1482279137c9cdd11e864d25a05d0a5
cddolanc <cddolanc@gmail.com>
2021-03-09 08:08:25 +0000

    local directory files dataframe complete

--- local_directory_scan.py
+++ local_directory_scan.py
@@ -12,7 +12,7 @@ import warnings
 import glob
 import numpy as np
 warnings.filterwarnings('ignore') # We can suppress the warnings
-
+pd.set_option('display.max_colwidth', -1) #Turn off the truncating display option
 
 # git_df = pd.DataFrame() # Define the dataframe
 # pd.set_option('display.max_colwidth', -1) #Turn off the truncating display option
@@ -70,11 +70,50 @@ for filename3 in glob.iglob(root_dir + '**/*.md', recursive=True):
     git_slides_df = git_slides_df.append({'Slides' : filename3},ignore_index = True)
 
 
-git_index_df = git_index_df.sort_index(ascending = True)
+
 print(git_index_df)
 
+git_index_df['week_no'] = None
+
+index_Index = git_index_df.columns.get_loc('Index')
+index_week_no = git_index_df.columns.get_loc('week_no')
+print(index_Index, index_week_no)
+pattern=r'(?:wk\d+)'
+for row in range(0, len(git_index_df)):
+    week = re.search(pattern, git_index_df.iat[row, index_Index]).group()
+    git_index_df.iat[row, index_week_no] =week
+
+git_index_df.sort_values(by=['week_no'], inplace=True)
+print(git_index_df)
+
+
+
+git_pdf_df['week_no1'] = None
+
+index_PDF = git_pdf_df.columns.get_loc('PDF')
+index_week_no1 = git_pdf_df.columns.get_loc('week_no1')
+print(index_PDF, index_week_no1)
+pattern=r'(?:wk\d+)'
+for row in range(0, len(git_pdf_df)):
+    week1 = re.search(pattern, git_pdf_df.iat[row, index_PDF]).group()
+    git_pdf_df.iat[row, index_week_no1] =week1
+
+git_pdf_df.sort_values(by=['week_no1'], inplace=True)
 print(git_pdf_df)
 
+
+
+git_slides_df['week_no2'] = None
+
+index_Slides = git_slides_df.columns.get_loc('Slides')
+index_week_no2 = git_slides_df.columns.get_loc('week_no2')
+print(index_Slides, index_week_no2)
+pattern=r'(?:wk\d+)'
+for row in range(0, len(git_slides_df)):
+    week2 = re.search(pattern, git_slides_df.iat[row, index_Slides]).group()
+    git_slides_df.iat[row, index_week_no2] =week2
+
+git_slides_df.sort_values(by=['week_no2'], inplace=True)
 print(git_slides_df)
 
 
@@ -100,9 +139,9 @@ print(df_git_pull)
 # s.apply(fetch_num)
 
 # print(s)
+df_git_pull = df_git_pull.drop(columns=['week_no1','week_no2'])
+df_git_pull = df_git_pull[['Index', 'PDF', 'Slides', 'week_no']]
 
+# git_index_df = Index.sort_values(by = 'Name')
 
-
-git_index_df = Index.sort_values(by = 'Name')
-
-print(git_index_df)
+print(df_git_pull)
